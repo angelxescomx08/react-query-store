d44df5e79fb70030b9448ea7ddae59fbee0c8448
Jose Angel <ah@xdevelop.mx>
2024-09-15 16:15:04 -0600

    instalar react query

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,18 @@ import ReactDOM from 'react-dom/client'
 import { NextUIProvider } from "@nextui-org/react";
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router/router.tsx';
+import { TanstackProvider } from './plugins';
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <NextUIProvider>
-      <main className="dark text-foreground bg-background">
-        <RouterProvider router={ router } />
-      </main>
-    </NextUIProvider>
+    <TanstackProvider>
+      <NextUIProvider>
+        <main className="dark text-foreground bg-background">
+          <RouterProvider router={ router } />
+        </main>
+      </NextUIProvider>
+    </TanstackProvider>
   </React.StrictMode>,
 )
