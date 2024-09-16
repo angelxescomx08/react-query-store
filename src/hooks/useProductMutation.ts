cba8dd55c81f97226efd279a2ca3272c5c5f8700
Jose Angel <ah@xdevelop.mx>
2024-09-16 10:53:27 -0600

    cambios

--- src/hooks/useProductMutation.ts
+++ src/hooks/useProductMutation.ts
@@ -31,13 +31,41 @@ export const useProductMutation = () => {
           return [...oldData, optimisticProduct];
         }
       );
+
+      return {
+        optimisticProduct
+      };
     },
-    onError: () => {
-      console.log('Product creation failed');
+    onSuccess: (data, variables, context) => {
+      queryClient.removeQueries({
+        queryKey: ['products', { filterKey: context.optimisticProduct.category }]
+      })
+      queryClient.setQueryData<Product[]>(
+        ['products', { filterKey: data.category }],
+        (oldData) => {
+          if (!oldData) return [data];
+          return oldData.map((product) => {
+            if (product.id === context.optimisticProduct.id) {
+              return data;
+            }
+            return product;
+          });
+        }
+      );
+    },
+    onError: (data, variables, context) => {
+      queryClient.setQueryData<Product[]>(
+        ['products', { filterKey: context?.optimisticProduct.category }],
+        (oldData) => {
+          if (!oldData) return [];
+          return oldData.filter((product) => product.id !== context?.optimisticProduct.id);
+        }
+      );
     },
     onSettled: () => {
       console.log('Product creation completed');
-    }
+    },
+    
   });
 
   return {
